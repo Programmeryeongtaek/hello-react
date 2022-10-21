02ed6a2fa6ca7b9923c2e01b88450640e9325a1b
오영택 <87858689+Programmeryeongtaek@users.noreply.github.com>
2022-10-21 23:58:27 +0900

    20221021

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,31 @@
 import { Component } from "react";
-import ValidationSample from "./App5.2.ValidationSample";
+import LifeCycleSample from "./App7.3.LifeCycleSample";
+import ErrorBoundary from "./App7.3.ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
