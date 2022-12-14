ee6e5b1c3a7d960226e5fd304e1a91bbc6586911
YeongTaek <yeongtaek2109@gmail.com>
2022-12-15 01:58:48 +0900

    221214

--- DreamCoding/todo/src/components/Todo/Todo.jsx
+++ DreamCoding/todo/src/components/Todo/Todo.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { FaRegTrashAlt } from 'react-icons/fa';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
+  const { text, status } = todo;
+  const handleChange = (e) => {
+    const status = e.target.checked ? 'completed' : 'active';
+    onUpdate({ ...todo, status });
+  };
+  const handleDelet = () => onDelete(todo);
+
   return (
     <li>
-      <input type="checkbox" id='checkbox' />
-      <label htmlFor='checkbox'>{todo.text}</label>
-      <button></button>
+      <input 
+        type="checkbox" 
+        id='checkbox' 
+        checked={status === 'completed'}
+        onChange={handleChange}
+      />
+      <label htmlFor='checkbox'>{text}</label>
+      <button onClick={handleDelet}>
+        <FaRegTrashAlt/>
+      </button>
     </li>
   );
 }
